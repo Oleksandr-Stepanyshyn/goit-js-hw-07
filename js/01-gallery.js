feb3138fae8e0759786b019e7b261cc4040d607b
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-12 21:07:54 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const refs = {
+    galleryList: document.querySelector(".gallery")
+};
+console.log(refs.galleryList);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryList.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+}).join("");
+};
+
+console.log(galleryMarkup);
